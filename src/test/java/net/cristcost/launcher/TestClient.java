d9351b37855f9af30ba4fbc03f3fb1de123bb0c2
cristcost <cristiano.costantini@gmail.com>
2014-01-01 12:13:14 +0100

    added handling of authentication to the non-web test

--- src/test/java/net/cristcost/launcher/TestClient.java
+++ src/test/java/net/cristcost/launcher/TestClient.java
@@ -1,7 +1,11 @@
 package net.cristcost.launcher;
 
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,7 +14,13 @@ public class TestClient implements Runnable {
 
   private final static Logger logger = Logger.getLogger(TestClient.class.getName());
 
+  private AuthenticationManager authenticationManager = null;
+  
   private TestService service = null;
+
+  public AuthenticationManager getAuthenticationManager() {
+    return authenticationManager;
+  }
 
   public TestService getService() {
     return service;
@@ -18,12 +28,22 @@ public class TestClient implements Runnable {
 
   @Override
   public void run() {
+    
+    
     try {
       Thread.sleep(500);
     } catch (InterruptedException e) {
     }
     logger.info("The service is of class: " + service.getClass().getName());
 
+    SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_THREADLOCAL);
+    
+    UsernamePasswordAuthenticationToken authRequest =
+        new UsernamePasswordAuthenticationToken("admin", "123");
+       Authentication authentication = getAuthenticationManager().authenticate(authRequest);
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    
+    
     try {
       service.serviceOne();
     } catch (AuthenticationException | AccessDeniedException e) {
@@ -48,6 +68,10 @@ public class TestClient implements Runnable {
       logger.log(Level.WARNING, "Auth failed: " + e.getMessage() + " ("
           + e.getClass().getSimpleName() + ")");
     }
+  }
+
+  public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+    this.authenticationManager = authenticationManager;
   }
 
   public void setService(TestService service) {
